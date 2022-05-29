094d189329ff7c6c975e953dcaf462ac6ef2ed64
Jordan Halfman <jordanhalfman@gmail.com>
2022-05-29 07:42:15 -0700

    commit tree e22845c4a408810e9850768ea0771d0b3fa7cfaf

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const wordLength = word.length;
+  let isPalindrome = true;
+  for (let i = 0; i < wordLength/2; i++){
+    console.log(wordLength/2)
+    if (word[i] != word[wordLength-1-i]){
+      isPalindrome = false;
+      return isPalindrome;
+    }
+  }
+  return isPalindrome;
 }
 
 /* 
   Add your pseudocode here
+
+  Create boolean and set to true
+  Find length of word
+  compare first index to last index
+  iterate toward center index
+  if letters don't match, change boolean to false and break from check
+  return boolean
+
+
 */
 
 /*
   Add written explanation of your solution here
+  The function will take in a word, and compare the first letter to the last letter while moving
+  toward the center of the word.  If the letters do not match, the process will stop and a "false" value
+  will be returned.  If there are an odd number of letters, the middle letter should be compared to itself
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +42,31 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("boob"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("aa"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("biglongworddrowgnolgib"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("biglongwordtgdrowgnolgib"));
 }
 
 module.exports = isPalindrome;
